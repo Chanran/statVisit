b1bcbd54e118cdeb6f9eab894f1df407d48ea9e4
blue <rong650751@163.com>
2017-06-10 13:10:39 +0800

    增加数据库

---
+++ docs/stat.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2017-06-10 12:46:15
+-- 服务器版本： 5.7.17-0ubuntu0.16.04.1
+-- PHP Version: 5.6.30-7+deb.sury.org~xenial+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL COMMENT 'log id',
+  `ip` varchar(255) CHARACTER SET utf8 NOT NULL COMMENT '访问者的ip',
+  `log_text` text CHARACTER SET utf8 NOT NULL COMMENT '日志内容',
+  `time` datetime NOT NULL COMMENT '日志记录时间'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'log id';
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
